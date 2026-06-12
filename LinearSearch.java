8c9696d12c9142b4da33a5f80b05f12ac1886f7b
Himanshu Upadhyay <himanshuupadhyay740@gmail.com>
2026-06-13 00:22:35 +0530

    Add files via upload

---
+++ LinearSearch.java
@@ -0,0 +1,12 @@
+package striver;
+
+public class LinearSearch {
+    public int linearSearch(int nums[], int target) {
+		//Your code goes here
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] == target){
+                return i;
+            }
+        }
+        return -1;
+}}
