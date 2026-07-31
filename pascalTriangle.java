1204e3abe14b1333ea9a2d3e029c98b095da5c35
Himanshu Upadhyay <himanshuupadhyay740@gmail.com>
2026-07-31 18:15:04 +0530

    Implement Pascal's Triangle calculation method

---
+++ pascalTriangle.java
@@ -0,0 +1,12 @@
+class pascalTriangle {
+    public int pascalTriangleI(int r, int c) {
+        int n = r-1;
+        int k = c -1;
+        int res = 1;
+        for(int i = 0; i < k; i++){
+            res = res * (n-i);
+            res = res / (i+1);
+        }
+        return res;
+    }
+}
